46c4206844fd8c72a11dab081766009d5b9edc95
malik250danish <danish250ahmad@gmail.com>
2025-03-12 02:03:53 +0500

    commit for restatng server change vue.config.js   addd public path

--- vue.config.js
+++ vue.config.js
@@ -13,15 +13,16 @@
 //   },
 // });
 
-const { defineConfig } = require('@vue/cli-service');
+const { defineConfig } = require("@vue/cli-service");
 
 module.exports = defineConfig({
   transpileDependencies: true,
+  publicPath: "/", // ✅ Important for AWS
   devServer: {
     port: 8081,
     proxy: {
-      '/api': {
-        target: 'https://52.59.240.119',
+      "/api": {
+        target: "https://52.59.240.119",
         changeOrigin: true,
         secure: false, // Disable SSL verification for development
       },
