f4dd948ae560762ead19ca38d7ec3a0f92ccf26e
malik250danish <danish250ahmad@gmail.com>
2025-03-18 04:34:58 +0500

    change baseurl

--- src/http-common.js
+++ src/http-common.js
@@ -6,8 +6,8 @@ export default axios.create({
   // origional url
   // baseURL: "https://king-prawn-app-3rw3o.ondigitalocean.app/api",
   // for development purpose
-  // baseURL: "https://king-prawn-app-3rw3o.ondigitalocean.app/api",
-  baseURL: "https://backend.revvdout.com/api",
+  baseURL: "https://king-prawn-app-3rw3o.ondigitalocean.app/api",
+  // baseURL: "https://backend.revvdout.com/api",
   // baseURL: "https://3.75.88.121/api",
   // adding header
   // rejectUnauthorized: false,
