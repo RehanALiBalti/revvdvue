38635bf2044f3b7e39e793d4da024bf665562ab0
malik250danish <danish250ahmad@gmail.com>
2025-03-21 00:07:20 +0500

    call thisfunction   this.fetchProfileData() after  submit story

--- src/components/HomeLanding.vue
+++ src/components/HomeLanding.vue
@@ -112,7 +112,7 @@
                 <router-link class="carContent row align-items-center" to="/banner" style="cursor:pointer">
                   <div class="col-md-5">
                     <h5 class="h5-title text-capitalize mb-2">{{ $t('Featured') }} <span class="coloror">{{ $t('Story')
-                        }}</span></h5>
+                    }}</span></h5>
                     <div class="">
                       <!-- <img :src="'https://king-prawn-app-3rw3o.ondigitalocean.app/stories/' + bannerStories[0].images[0]"
                       class="img-fluid" alt="car" v-if="bannerStories[0]?.images.length > 0" /> -->
@@ -3177,6 +3177,7 @@ export default {
           this.resetForm();
           this.loading = false;
           this.ModalStorySucces = true;
+          this.fetchProfileData()
         } catch (error) {
           console.error("Error making post request:", error);
           this.modalTitle = "Something went wrong";
