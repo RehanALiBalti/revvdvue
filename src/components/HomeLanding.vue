ca5fc641fc42522c850daea3f809849e54a2708d
malik250danish <danish250ahmad@gmail.com>
2025-03-26 02:39:58 +0500

    style change at 768 topN37 at home page

--- src/components/HomeLanding.vue
+++ src/components/HomeLanding.vue
@@ -112,7 +112,7 @@
                 <router-link class="carContent row align-items-center" to="/banner" style="cursor:pointer">
                   <div class="col-md-5">
                     <h5 class="h5-title text-capitalize mb-2">{{ $t('Featured') }} <span class="coloror">{{ $t('Story')
-                    }}</span></h5>
+                        }}</span></h5>
                     <div class="">
                       <!-- <img :src="'https://king-prawn-app-3rw3o.ondigitalocean.app/stories/' + bannerStories[0].images[0]"
                       class="img-fluid" alt="car" v-if="bannerStories[0]?.images.length > 0" /> -->
@@ -3975,6 +3975,14 @@ textarea.form-control {
 @media(max-width:816px) {
   .topN37 {
     top: -29px
+  }
+
+
+}
+
+@media(max-width:768px) {
+  .topN37 {
+    top: -35px
   }
 
 
