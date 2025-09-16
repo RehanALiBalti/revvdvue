6c2817cc3cf0c002c366fab2eee174a903765b20
malik250danish <danish250ahmad@gmail.com>
2025-09-16 19:09:21 +0500

    new changes on carlisting details vue page

--- src/components/CarsListingDetails.vue
+++ src/components/CarsListingDetails.vue
@@ -439,18 +439,21 @@ const form = reactive({
 }
 
 .top-icons {
-  top: 21%
+  top: 21%;
+  right: 1%
 }
 
 @media screen and (width: 1435px) and (min-height: 678px) {
   .top-icons {
-    top: 21%
+    top: 21%;
+    right: 1%
   }
 }
 
 @media screen and (width: 1905px) and (min-height:957px) {
   .top-icons {
-    top: 21%
+    top: 21%;
+    right: 1%
   }
 }
 
