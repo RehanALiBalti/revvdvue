7c9b558e3d3c80f832010e8202b864c8596ea188
malik250danish <danish250ahmad@gmail.com>
2025-09-19 18:46:28 +0500

    adding make in buy cr4

--- src/components/buyCar4.vue
+++ src/components/buyCar4.vue
@@ -151,7 +151,18 @@
                     <div class="modal-dialog ">
                         <div class="modal-content px-3 py-2">
                             <p class="text-white text-center mb-2">MG</p>
+                            <v-select v-model="formData.make" class="my-2 fsel" :options="makefilteredOptions"
+                                placeholder="Select a Make" :filterable="true">
+                                <template #open-indicator>
 
+                                    <img class="indicator_Image"
+                                        src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABMAAAAMCAYAAACA0IaCAAAAAXNSR0IArs4c6QAAAARzQklUCAgICHwIZIgAAADiSURBVChTjZIBEcIwDEVbBeAAHAAOOgk4YA7ACShgFlDAHAAOkAAKxv9cwqWl3dq7XbMk/+03nR+GYe6cu+Lharz3L4knN2jXou2h23ok7kisRMm4CmhANMN1IuyJYGFsTALlNDdolkZ3IYxWezyzGqAZC3W6HgiC51sByDk0dnAF0JsOOesvbATYoamVul6UdURQQA9H434wEQTserP6nQ7BQfJF0B9MgDvsZ3s8xPxd9NZYihxpb+TMHDkH1HIWlHVWAdzojBL38czSIm4vddgCxBlmV/aYtlOAR+T2YyBqPrkMZFSDhkgfAAAAAElFTkSuQmCC"
+                                        width="11px" />
+                                </template>
+                                <template #no-options>
+                                    <span>No options available</span>
+                                </template>
+                            </v-select>
                             <v-select v-model="formData.model" class="my-2 fsel" :options="modelfilteredOptions"
                                 placeholder="Select a Model" :filterable="true" :readonly="formData.make === ''">
                                 <template #open-indicator>
@@ -173,10 +184,10 @@
                     <div class="modal-dialog ">
                         <div class="modal-content px-3 py-2">
                             <p class="text-white text-center mb-2">NIssan</p>
-                            <!-- <v-select v-model="formData.make" class="my-2 fsel" :options="makefilteredOptions"
+                            <v-select v-model="formData.make" class="my-2 fsel" :options="makefilteredOptions"
                                 placeholder="Select a Make" :filterable="true">
                                 <template #open-indicator>
-                                   
+
                                     <img class="indicator_Image"
                                         src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABMAAAAMCAYAAACA0IaCAAAAAXNSR0IArs4c6QAAAARzQklUCAgICHwIZIgAAADiSURBVChTjZIBEcIwDEVbBeAAHAAOOgk4YA7ACShgFlDAHAAOkAAKxv9cwqWl3dq7XbMk/+03nR+GYe6cu+Lharz3L4knN2jXou2h23ok7kisRMm4CmhANMN1IuyJYGFsTALlNDdolkZ3IYxWezyzGqAZC3W6HgiC51sByDk0dnAF0JsOOesvbATYoamVul6UdURQQA9H434wEQTserP6nQ7BQfJF0B9MgDvsZ3s8xPxd9NZYihxpb+TMHDkH1HIWlHVWAdzojBL38czSIm4vddgCxBlmV/aYtlOAR+T2YyBqPrkMZFSDhkgfAAAAAElFTkSuQmCC"
                                         width="11px" />
@@ -184,7 +195,7 @@
                                 <template #no-options>
                                     <span>No options available</span>
                                 </template>
-                            </v-select> -->
+                            </v-select>
                             <v-select v-model="formData.model" class="my-2 fsel" :options="modelfilteredOptions"
                                 placeholder="Select a Model" :filterable="true" :readonly="formData.make === ''">
                                 <template #open-indicator>
@@ -207,10 +218,10 @@
                         <div class="modal-content px-3 py-2">
                             <p class="text-white text-center mb-2">Mercedes
                             </p>
-                            <!-- <v-select v-model="formData.make" class="my-2 fsel" :options="makefilteredOptions"
+                            <v-select v-model="formData.make" class="my-2 fsel" :options="makefilteredOptions"
                                 placeholder="Select a Make" :filterable="true">
                                 <template #open-indicator>
-                                
+
                                     <img class="indicator_Image"
                                         src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABMAAAAMCAYAAACA0IaCAAAAAXNSR0IArs4c6QAAAARzQklUCAgICHwIZIgAAADiSURBVChTjZIBEcIwDEVbBeAAHAAOOgk4YA7ACShgFlDAHAAOkAAKxv9cwqWl3dq7XbMk/+03nR+GYe6cu+Lharz3L4knN2jXou2h23ok7kisRMm4CmhANMN1IuyJYGFsTALlNDdolkZ3IYxWezyzGqAZC3W6HgiC51sByDk0dnAF0JsOOesvbATYoamVul6UdURQQA9H434wEQTserP6nQ7BQfJF0B9MgDvsZ3s8xPxd9NZYihxpb+TMHDkH1HIWlHVWAdzojBL38czSIm4vddgCxBlmV/aYtlOAR+T2YyBqPrkMZFSDhkgfAAAAAElFTkSuQmCC"
                                         width="11px" />
@@ -218,7 +229,7 @@
                                 <template #no-options>
                                     <span>No options available</span>
                                 </template>
-                            </v-select> -->
+                            </v-select>
                             <v-select v-model="formData.model" class="my-2 fsel" :options="modelfilteredOptions"
                                 placeholder="Select a Model" :filterable="true" :readonly="formData.make === ''">
                                 <template #open-indicator>
